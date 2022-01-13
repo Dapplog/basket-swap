b1f87345d4f685cfe3c59e85d480086066e0a9f0
Daniel Visca <danielvisca96@gmail.com>
2022-01-12 22:17:41 -0800

    wallet asks to connect or if previously connected, autoconnects

--- apps/web/src/pages/home/swap/views/wallet/connect/list/List.jsx
+++ apps/web/src/pages/home/swap/views/wallet/connect/list/List.jsx
@@ -1,16 +1,18 @@
-import  React, { useEffect } from 'react';
+import  React, { useEffect, useState } from 'react';
 import { _list, _item, _bubble, _icon, _name } from './List.styled';
 import MathWallet from 'design/icons/math_wallet/MathWallet';
 import Ledger from 'design/icons/ledger/Ledger';
 import { useTranslation } from 'react-i18next';
 import Phantom from 'design/icons/phantom/Phantom';
 import { COLOR_LEDGER, COLOR_MATH, COLOR_PHANTOM } from 'design/colors/colors';
-import usePhantomConnect from 'core/hooks/usePhantomConnect';
+// import usePhantomConnect from 'core/hooks/usePhantomConnect';
 const log = console.log;
 
 export const List = () => {
   const { t } = useTranslation();
-  // const { usePhantomConnect } = usePhantomConnect();
+
+  const [walletAddress, setWalletAddress] = useState(null);
+  
   const checkIfWalletIsConnected = async () => {
     try {
       const { solana } = window;
@@ -18,15 +20,49 @@ export const List = () => {
       if (solana) {
         if (solana.isPhantom) {
           console.log('Phantom wallet found!');
+          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log(
+            'Connected with Public Key:',
+            response.publicKey.toString()
+          );
+          setWalletAddress(response.publicKey.toString());
         }
-      } else {
-        alert('Solana object not found! Get a Phantom Wallet 👻');
       }
     } catch (error) {
       console.error(error);
     }
   };
 
+  const connectWallet = async () => {
+    const { solana } = window;
+  
+    if (solana) {
+      const response = await solana.connect();
+      console.log('Connected with Public Key:', response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  // const renderPhantomConnectButton = () => {
+  //   <button onClick={() => connectWallet()}>
+  //     <_item >
+  //       <_bubble $logo={COLOR_PHANTOM} >
+  //         <_icon $logo={COLOR_PHANTOM} >{Phantom()}</_icon>
+  //       </_bubble>
+  //       <_name>{"Connect to Phantom"}</_name>
+  //     </_item>
+  //   </button>
+  // }
+
+  useEffect(() => {
+    const onLoad = async () => {
+      log("\n\nUSING EFFECT\n\n")
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+  
   return (
     <_list>
       <_item>
@@ -35,14 +71,14 @@ export const List = () => {
         </_bubble>
         <_name>{t('wallet.coming.soon')}</_name>
       </_item>
-      <button onClick={() => checkIfWalletIsConnected()}>
-        <_item >
-          <_bubble $logo={COLOR_PHANTOM} >
-            <_icon $logo={COLOR_PHANTOM} >{Phantom()}</_icon>
-          </_bubble>
-          <_name>{t('wallet.coming.soon')}</_name>
-        </_item>
-      </button>
+      { !walletAddress && <button onClick={() => connectWallet()}>
+      <_item >
+        <_bubble $logo={COLOR_PHANTOM} >
+          <_icon $logo={COLOR_PHANTOM} >{Phantom()}</_icon>
+        </_bubble>
+        <_name>{"Connect to Phantom"}</_name>
+      </_item>
+    </button>}
       <_item>
         <_bubble $logo={COLOR_LEDGER}>
           <_icon $logo={COLOR_LEDGER}>{Ledger()}</_icon>
