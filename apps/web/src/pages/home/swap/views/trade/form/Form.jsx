d5a1b48dbec714f7fb68560b1afab8f68bc63ae7
Daniel Visca <danielvisca96@gmail.com>
2022-02-02 19:34:13 -0800

    work in progress serum connection

--- apps/web/src/pages/home/swap/views/trade/form/Form.jsx
+++ apps/web/src/pages/home/swap/views/trade/form/Form.jsx
@@ -1,31 +1,77 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useLayoutEffect } from 'react';
 import { _form, _section, _weights } from './Form.styled';
 import Percent from '../../../../../../shared/percent/Percent';
 import Input from '../../../../../../shared/forms/input/Input';
-
+// import Swap from './Serum.swp.js';
 import { Account, Connection, PublicKey } from '@solana/web3.js';
-import { Market } from '@project-serum/serum';
+import { Market, MARKETS } from '@project-serum/serum';
 let log = console.log;
 
 export const Form = () => {
-  const Swap = async () => {
+  let allMarkets = [
+    "9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin",
+    "GekRdc4eD9qnfPTjUMK5NdQDho8D9ByGrtnqhMNCTm36",
+    "HkLEttvwk2b4QDAHzNcVtxsvBG35L1gmYY4pecF9LrFe",
+    "CRW23rK5LksqowrfpQTEUVEFUDNDLF34STyW7gXV18Xt",
+  ]
+  for (let marketId in allMarkets) {
+    
+    log(marketId, MARKETS.find(
+      (m) =>
+        m.address.toBase58() === marketId && m.programId.toBase58() === programId,
+    ));
+    }
+  let marketId = new PublicKey('9wFFyRfZBsuAha4YcuxcXLKwMxJR43S7fPfQLusDBzvT')
+  let programId = new PublicKey('9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin')
+  let connection = new Connection('https://api.mainnet-beta.solana.com');
+  log("Connection: ", connection)
+  let market = null;
+  const knownMarket = MARKETS.find(
+    (m) =>
+      m.address.toBase58() === marketId.toBase58() && m.programId.toBase58() === programId.toBase58(),
+  );
+  log("knownMarket:", knownMarket)
+  const knownProgram = MARKETS.find(
+    (m) => m.programId.toBase58() === programId.toBase58(),
+  );
+  log("knownProgram: ",knownProgram)
+  useLayoutEffect(() => {
+    (async() => {
+      log('cool');
+      log(await connection.getAccountInfo(marketId));
+      log('good');
+      if (window.fetch) Market.load(
+        // new Connection('http://localhost:8899')
+        // new Connection('https://devnet.solana.com')
+        // new Connection('https://api.devnet.solana.com')
+        connection
+        ,
+        marketId,
+        {},
+        programId,
+      )
+        .then((m) => {
+          log("setting market")
+          log(m);
+          market = m;
+        })
+        .catch((e) => {
+          log("market failed", e)
+        })
+      log("just after market set")
+    })()
+    
+    }, []);
+
+  const Swap = async (market) => {
     log("trying to serum swap")
     let localhostPhantomWalletAddress = "9oTpBeexxPmLGwjQFHeQhmFrYg3ouvxQ7ryyHGXwHATn";
     let wallet4PhantomWalletAddress = "HdyGV8mN3tqGwxiH4bvSW62uBpRGPyodaZTY3gfuG69U";
-    let solusdcMarket = "9wFFyRfZBsuAha4YcuxcXLKwMxJR43S7fPfQLusDBzvT";
-    let solEth = "HkLEttvwk2b4QDAHzNcVtxsvBG35L1gmYY4pecF9LrFe"
-    let testMarket = "CRW23rK5LksqowrfpQTEUVEFUDNDLF34STyW7gXV18Xt"
-    let connection = new Connection('https://localhost');
-    // let connection = new Connection('https://devnet.solana.com');
-    let marketAddress = new PublicKey(testMarket);
-    // try {
-    //   let market = await Market.load(connection, marketAddress);
-    // } catch {
-    //   log("getting market info didnt work")
-    // }
-    let market = await Market.load(connection, marketAddress);
 
-
+    log("just before market set")
+    // log(connection)
+    // let market = await Market.load(connection, marketAddress).catch(log("the big bad"))
+    log(market)
     // Fetching orderbooks
     let bids = await market.loadBids(connection);
     let asks = await market.loadAsks(connection);
@@ -120,5 +166,4 @@ export const Form = () => {
     watch,
   );
 };
-
 export default Form;
